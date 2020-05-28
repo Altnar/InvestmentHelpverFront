a2ec31a50f1d2fe18c53ffb0797f19d0b5d0d17c
Altnar <wrk.Altnar@gmail.com>
2020-05-28 19:37:31 +0500

    change port

--- main.go
+++ main.go
@@ -6,8 +6,8 @@ import (
 )
 
 func main() {
-	port := "3000"
-	fmt.Println(fmt.Sprintf("WebPage Addr: http://localhost:%s/mainPage.html", port))
+	port := ":3000"
+	fmt.Println(fmt.Sprintf("WebPage Addr: http://localhost%s/mainPage.html", port))
 	http.Handle("/", http.FileServer(http.Dir("./static")))
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(port, nil)
 }
